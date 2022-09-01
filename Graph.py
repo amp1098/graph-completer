291379a3962b615c4744e295d77e451303d1d437
amp1098 <86174743+amp1098@users.noreply.github.com>
2022-08-31 21:23:45 -0400

    Added new "average length" plotting function
    You can now generate a scatter plot of the average lengths of the random lines drawn in the figures.

--- Graph.py
+++ Graph.py
@@ -30,14 +30,18 @@ rand_var = tk.BooleanVar(name="Random Generation", value=False)
 circ_var = tk.BooleanVar(name="Circle Lock", value=True)
 point_var = tk.StringVar(name="Number of Points", value="6")
 width_var = tk.StringVar(name="Width of Lines", value="0.003")
+x_l_lim_var = tk.StringVar(name="x lower lim", value="-1")
+x_u_lim_var = tk.StringVar(name="x upper lim", value="1")
+y_l_lim_var = tk.StringVar(name="y lower lim", value="-1")
+y_u_lim_var = tk.StringVar(name="y upper lim", value="1")
 
 # --- labels and fields ---
-label_names = ["Number of Points", "Line Width"]
+label_names = ["Number of Points", "Line Width", "x lower limit", "x upper limit", "y lower limit", "y upper limit"]
 
 for i in label_names:
     tk.Label(frame, text=i).grid(sticky="W", row=label_names.index(i), column=0)
 
-text_vars = [point_var, width_var]
+text_vars = [point_var, width_var, x_l_lim_var, x_u_lim_var, y_l_lim_var, y_u_lim_var]
 
 for i in text_vars:
     tk.Entry(frame, textvariable=i, width=10).grid(sticky="W", row=text_vars.index(i), column=1)
@@ -63,13 +67,60 @@ def t_point(p, s_1, s_2):  # transforms numbers (p) from s_1 = [a,b] to s_2 = [c
     return (((p - s_1[0]) * (s_2[1] - s_2[0])) / (s_1[1] - s_1[0])) + s_2[0]
 
 
-def avg(s): # finds mean of a list
+def avg(s):  # finds mean of a list
     count = 0
     total = 0
     while count < len(s):
         total += s[count]
         count += 1
     return total / len(s)
+
+
+def lineavg(val):
+    rand_gui = rand_var.get()
+    circ_gui = circ_var.get()
+    x_l_lim_gui = int(x_l_lim_var.get())
+    x_u_lim_gui = int(x_u_lim_var.get())
+    y_l_lim_gui = int(y_l_lim_var.get())
+    y_u_lim_gui = int(y_u_lim_var.get())
+
+
+    counting = 0
+
+    x_list = (int(val) + counting) * [0]
+    y_list = (int(val) + counting) * [0]
+
+    if rand_gui and circ_gui:
+        for i in range(0, len(x_list)):
+            t = np.random.uniform(0, 2 * np.pi)
+            x_list[i] = np.cos(t) * x_u_lim_gui
+            y_list[i] = np.sin(t) * y_u_lim_gui
+    if (rand_gui is True) and (circ_gui is False):
+        for i in range(0, len(x_list)):
+            x_list[i] = np.random.uniform(x_l_lim_gui, x_u_lim_gui)
+            y_list[i] = np.random.uniform(y_l_lim_gui, y_u_lim_gui)
+    if (rand_gui is False) and (circ_gui is True):
+        for i in range(0, len(x_list)):
+            t = t_point(i, [0, len(x_list)], [0, 2 * np.pi])
+            x_list[i] = np.cos(t) * x_u_lim_gui
+            y_list[i] = np.sin(t) * y_u_lim_gui
+    if (rand_gui is False) and (circ_gui is False):
+        print("Please have at least one checkbox selected.")
+
+    lengths = []
+    p = 0
+    while p <= len(x_list):
+        q = len(x_list)
+        while q >= 0:
+            distV = np.sqrt(
+                ((x_list[p - 1] - x_list[q - 1]) * (x_list[p - 1] - x_list[q - 1])) +
+                (y_list[p - 1] - y_list[q - 1]) * (y_list[p - 1] - y_list[q - 1])
+            )  # distance of each line as it is generated
+            lengths.append(distV)
+            q -= 1
+        p += 1
+
+    return avg(lengths)
 
 
 # --- update function ---
@@ -83,6 +134,10 @@ def update():
     point_gui = point_var.get()
     width_gui = width_var.get()
     circ_gui = circ_var.get()
+    x_l_lim_gui = int(x_l_lim_var.get())
+    x_u_lim_gui = int(x_u_lim_var.get())
+    y_l_lim_gui = int(y_l_lim_var.get())
+    y_u_lim_gui = int(y_u_lim_var.get())
 
     x_list = (int(point_gui) + counting) * [0]
     y_list = (int(point_gui) + counting) * [0]
@@ -91,17 +146,17 @@ def update():
     if rand_gui and circ_gui:
         for i in range(0, len(x_list)):
             t = np.random.uniform(0, 2 * np.pi)
-            x_list[i] = np.cos(t) * 0.8
-            y_list[i] = np.sin(t) * 0.8
+            x_list[i] = np.cos(t) * x_u_lim_gui
+            y_list[i] = np.sin(t) * y_u_lim_gui
     if (rand_gui is True) and (circ_gui is False):
         for i in range(0, len(x_list)):
-            x_list[i] = np.random.uniform(-0.8, 0.8)
-            y_list[i] = np.random.uniform(-0.8, 0.8)
+            x_list[i] = np.random.uniform(x_l_lim_gui, x_u_lim_gui)
+            y_list[i] = np.random.uniform(y_l_lim_gui, y_u_lim_gui)
     if (rand_gui is False) and (circ_gui is True):
         for i in range(0, len(x_list)):
             t = t_point(i, [0, len(x_list)], [0, 2 * np.pi])
-            x_list[i] = np.cos(t) * 0.8
-            y_list[i] = np.sin(t) * 0.8
+            x_list[i] = np.cos(t) * x_u_lim_gui
+            y_list[i] = np.sin(t) * y_u_lim_gui
     if (rand_gui is False) and (circ_gui is False):
         print("Please have at least one checkbox selected.")
 
@@ -113,7 +168,7 @@ def update():
     splot.set_xlim([-1, 1])
     splot.set_ylim([-1, 1])
 
-    splot.scatter(x_list, y_list, color='blue')
+    splot.scatter(x_list, y_list, color='blue', s=10)
 
     # setting up colormap for lines
     cmap = cm.get_cmap('hot_r')
@@ -144,6 +199,50 @@ def update():
         p += 1
 
     print(avg(lengths))
+
+    canvas.draw()
+    canvas.get_tk_widget().grid(row=0, column=0)
+
+
+def avgplot():
+    point_gui = int(point_var.get())
+    x_l_lim_gui = int(x_l_lim_var.get())
+    x_u_lim_gui = int(x_u_lim_var.get())
+    y_l_lim_gui = int(y_l_lim_var.get())
+    y_u_lim_gui = int(y_u_lim_var.get())
+    circ_gui = circ_var.get()
+
+    fig.clear()
+
+    x_range = range(1, point_gui)
+    y_range = []
+    y_range2 = []
+
+    for element in x_range:
+        y_range.append(lineavg(element))
+
+    distx = abs(x_u_lim_gui) + abs(x_l_lim_gui)
+    disty = abs(y_u_lim_gui) + abs(y_l_lim_gui)
+
+    splot = fig.add_subplot()
+    # splot.axis('Equal')
+    splot.grid(False)
+    if circ_gui:
+        splot.title.set_text("Average Length of Random Line in " + str(distx) + "x" + str(disty) + " Oval")
+    else:
+        splot.title.set_text("Average Length of Random Line in " + str(distx) + "x" + str(disty) + " Box")
+    # splot.axis('off')
+    splot.set_xlabel("# of Points")
+    splot.set_ylabel("Length")
+    splot.set_xlim([-1, point_gui + 1])
+    splot.set_ylim([-1, y_u_lim_gui + 1])
+
+    splot.scatter(x_range, y_range, color='blue', s=10)
+
+    for element in x_range:  # creating a constant function to estimate convergence
+        y_range2.append(np.sqrt(np.pi / 2))
+
+    splot.scatter(x_range, y_range2, s=5)
 
     canvas.draw()
     canvas.get_tk_widget().grid(row=0, column=0)
@@ -189,7 +288,10 @@ plus_button.grid(row=0, column=2)
 minus_button = Button(master=frame, text="-", command=minus_points)
 minus_button.grid(row=0, column=3)
 
+avgplot_button = Button(master=frame, text="AvgPlot", command=avgplot)
+avgplot_button.grid(row=len(text_vars) + len(check_vars) + 1, column=0)
+
 savefig_button = Button(master=frame, text="Save graph as .png", command=save_fig)
-savefig_button.grid(row=len(text_vars) + len(check_vars) + 1, column=0)
+savefig_button.grid(row=len(text_vars) + len(check_vars) + 2, column=0)
 
 root.mainloop()
